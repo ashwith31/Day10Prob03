35e3d0476e12fce2179874b9af60194bce79749e
ashwith31 <ashwitash@gmail.com>
2021-07-21 14:12:23 +0530

    modified:Singlet Percentage Calculated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,25 @@
 #! /bin/bash -x
-
+declare -A Singlet
+read -p "Enter the number of times you want to flip the coin:" a
+echo "H==Head"
+echo "T==Tail"
+h=0
+t=0
+for((i=0;i<$a;i++))
+do
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
-echo "Its heads"
+(( h++ ))
 else
-echo "Its tails"
+(( t++ ))
 fi
+done
+Singlet['H']=$h
+Singlet['T']=$t
+for key in ${!Singlet[*]}
+do
+p=$( echo ${Singlet[$key]}  $a | awk '{ print $1/$2*100 }' )
+echo "Percentage of $key win is $p%"
+done
+
