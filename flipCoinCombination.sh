7b423e154274b5e536efa42c386b321a53480e66
ashwith31 <ashwitash@gmail.com>
2021-07-21 15:04:13 +0530

    modified:Doublet percentage calculated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,11 @@
 #! /bin/bash -x
 declare -A Singlet
+declare -A Doublet
+declare -A Triplet
 read -p "Enter the number of times you want to flip the coin:" a
 echo "H==Head"
 echo "T==Tail"
+echo "**********Singlet*********"
 h=0
 t=0
 for((i=0;i<$a;i++))
@@ -19,7 +22,38 @@ Singlet['H']=$h
 Singlet['T']=$t
 for key in ${!Singlet[*]}
 do
-p=$( echo ${Singlet[$key]}  $a | awk '{ print $1/$2*100 }' )
-echo "Percentage of $key win is $p%"
+p1=$( echo ${Singlet[$key]}  $a | awk '{ print $1/$2*100 }' )
+echo "Percentage of $key win is $p1%"
 done
-
+echo " "
+echo "***********Doublet************"
+hh=0
+ht=0
+th=0
+tt=0
+for((i=0;i<$a;i++))
+do
+flip1=$(( RANDOM%2 ))
+flip2=$(( RANDOM%2 ))
+if [ $flip1 -eq 0 -a $flip2 -eq 0 ]
+then
+(( hh++ ))
+elif [ $flip1 -eq 0 -a $flip2 -eq 1 ]
+then
+(( ht++ ))
+elif [ $flip1 -eq 1 -a $flip2 -eq 0 ]
+then
+(( th++ ))
+else
+(( tt++ ))
+fi
+done
+Doublet['HH']=$hh
+Doublet['HT']=$ht
+Doublet['TH']=$th
+Doublet['TT']=$tt
+for key in ${!Doublet[*]}
+do
+p1=$( echo ${Doublet[$key]}  $a | awk '{ print $1/$2*100 }' )
+echo "Percentage of $key win is $p1%"
+done
